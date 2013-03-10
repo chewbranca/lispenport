2c93f1d1e05904b12fd4865a2f5fc5fc79f47dd9
Russell Branca <chewbranca@gmail.com>
2013-03-10 01:23:46 -0800

    Initial OTP file structure

--- src/lispenport_lang.erl
+++ src/lispenport_lang.erl
@@ -1,25 +1,25 @@
-%% Licensed under the Apache License, Version 2.0 (the "License"); you may not
-%% use this file except in compliance with the License. You may obtain a copy of
-%% the License at
-%%
-%%   http://www.apache.org/licenses/LICENSE-2.0
-%%
-%% Unless required by applicable law or agreed to in writing, software
-%% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-%% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-%% License for the specific language governing permissions and limitations under
-%% the License.
-%%
-%% Copyright (c) 2013 Russell Branca <@chewbranca>
-%%
-%%
-%% Experimentations with a CouchDB Lisp for views
-%% This version is a fork of:
-%%   http://norvig.com/lispy.html
-%%
+% Copyright (c) 2013 Russell Branca <@chewbranca>
+%
+% Licensed under the Apache License, Version 2.0 (the "License"); you may not
+% use this file except in compliance with the License. You may obtain a copy of
+% the License at
+%
+%   http://www.apache.org/licenses/LICENSE-2.0
+%
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+% License for the specific language governing permissions and limitations under
+% the License.
+%
+%
+% Experimentations with a CouchDB Lisp for views
+% This version is a fork of:
+%   http://norvig.com/lispy.html
+%
 
 
--module(lispenport).
+-module(lispenport_lang).
 -include_lib("eunit/include/eunit.hrl").
 
 
